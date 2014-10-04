f4a8883e0bd5fe72157beee19902405aa9197c43
Jonas <jonas.felix@icloud.com>
2014-10-04 17:44:13 +0200

    use $() to prevent grep from [] error

--- bitbucket-clone-all.sh
+++ bitbucket-clone-all.sh
@@ -3,7 +3,7 @@
 # Usage: bitbucket-clone-all.sh <username> <apikey>
 
 curl -u ${1}:${2}  https://api.bitbucket.org/1.0/users/${1} > repoinfo.json
-for repo_name in `grep -Po '"name":.*?[^\\]",' repoinfo.json |cut -f4 -d\"`
+for repo_name in $(grep -Po '"name":.*?[^\\]",' repoinfo.json |cut -f4 -d\")
 do
 	git clone --bare ssh://git@bitbucket.org/${1}/$repo_name
 done
