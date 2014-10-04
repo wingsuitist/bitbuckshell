64d85ced6a55aee7dac2ac9e85ded42d2283b7cb
cabvh029 <admin@cabag.ch>
2014-10-04 18:16:24 +0200

    clean netbeans file rename mess

--- bitbucket-clone-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to get all repositories under a user from bitbucket
-# Usage: bitbucket-clone-all.sh <username> <apikey>
-
-curl -u ${1}:${2}  https://api.bitbucket.org/1.0/users/${1} > repoinfo.json
-for repo_name in $(grep -Po '"name":.*?[^\\]",' repoinfo.json |cut -f4 -d\")
-do
-	git clone --bare ssh://git@bitbucket.org/${1}/$repo_name
-done
--- bitbucket-sync-all.sh
+++ bitbucket-sync-all.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-# Script to get all repositories from a bitbucket user and keep them in sync
+# Script to get all repositories under a user from bitbucket
+# Usage: bitbucket-clone-all.sh <username> <apikey>
 
-if [ -z "${1}" ] || [ -z "${2}" ]; then \
-	echo "usage: bitbucket-sync-all.sh <username> <apikey>"
-	exit;
-fi
-
-echo "fetching repoinfo.json"
-curl -u ${1}:${2}  https://api.bitbucket.org/1.0/users/${1} > repoinfo.json 2>/dev/null
-
+curl -u ${1}:${2}  https://api.bitbucket.org/1.0/users/${1} > repoinfo.json
 for repo_name in $(grep -Po '"name":.*?[^\\]",' repoinfo.json |cut -f4 -d\")
 do
-	if [ -d "$repo_name.git" ] ; then \
-		echo fetching "$repo_name"
-		git --git-dir="$repo_name.git" fetch -q --all -p
-	else \
-		git clone --bare "ssh://git@bitbucket.org/${1}/$repo_name"
-	fi
+	git clone --bare ssh://git@bitbucket.org/${1}/$repo_name
 done
