1b52c552ba3363048079a7454d21dc242f0e1ceb
Jonas Felix <jf@cabag.ch>
2015-08-03 09:49:14 +0200

    improves fetching of bitbucket repositories

--- bitbucket-sync-all.sh
+++ bitbucket-sync-all.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-# Script to get all repositories under a user from bitbucket
-# Usage: bitbucket-clone-all.sh <username> <apikey>
+# Script to get all repositories from a bitbucket user and keep them in sync
 
-curl -u ${1}:${2}  https://api.bitbucket.org/1.0/users/${1} > repoinfo.json
+if [ -z "${1}" ] || [ -z "${2}" ]; then \
+	echo "usage: bitbucket-sync-all.sh <username> <apikey>"
+	exit;
+fi
+
+echo "fetching repoinfo.json"
+curl -u ${1}:${2}  https://api.bitbucket.org/1.0/users/${1} > repoinfo.json 2>/dev/null
+
 for repo_name in $(grep -Po '"name":.*?[^\\]",' repoinfo.json |cut -f4 -d\")
 do
-	git clone --bare ssh://git@bitbucket.org/${1}/$repo_name
+	if [ -d "$repo_name.git" ] ; then \
+		echo fetching "$repo_name"
+		git --git-dir="$repo_name.git" fetch -q --all -p
+	else \
+		git clone --bare "ssh://git@bitbucket.org/${1}/$repo_name"
+	fi
 done
